225d3ef9ad82bd0ce48bd50ee2dccdc9e18283e7
Alex <89866117+TichinaBuckle@users.noreply.github.com>
2023-04-21 10:01:58 -0500

    Add files via upload

---
+++ Utech_Complaint_System_Server/src/server/Driver.java
@@ -0,0 +1,16 @@
+/*
+Aurthors: Tichina Buckle, Khi Lewis, Aviel Reid and Shemar Williams
+*/
+
+package server;
+
+public class Driver {
+
+	// Starts server
+	public static void main(String[] args) {
+		int port = 12345;
+        Server server = new Server(port);
+        server.start();
+	}
+
+}
